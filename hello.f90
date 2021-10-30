c930fb1fd80aea10596138f6d2b5ce03c05c16c6
zhuhua123 <zhulzy@stu.xjtu.edu.cn>
2021-10-30 19:33:40 +0800

    sub

---
+++ hello.f90
@@ -0,0 +1,25 @@
+PROGRAM add_numbers
+implicit none
+   integer::a(3,3),i
+do i=1,3
+    a(i,:)=2*i
+end do
+  open(12,file="g:\datte.csv")
+do i=1,3
+    write(12,"(3(i5,','))")  a(i,:)
+ 
+end do
+close(12)
+open(12,file="datte.csv")
+do i=1,3
+    write(12,"(3(i5,','))")  a(i,:)
+end do
+close(12)
+read(*,*)
+stop
+END
+SUBROUTINE ADD(A,B,C)
+    END SUBROUTINE
+    
+
+
